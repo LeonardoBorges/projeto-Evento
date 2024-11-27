03f393ed7bbc85678940b157bdfb112ba64d0627
Leonardo Borges <leo.nunes.borges@hotmail.com>
2024-11-27 17:59:51 -0300

    arquvio e configuração mobile

--- apps/frontend/src/app/(paginas)/layout.tsx
+++ apps/frontend/src/app/(paginas)/layout.tsx
@@ -1,5 +1,10 @@
 import Pagina from "@/components/template/Pagina";
+import { ProvedorContextoEvento } from "@/data/context/ContextoEvento";
 
 export default function Layout(props: any) {
-  return <Pagina>{props.children}</Pagina>;
+  return (
+    <ProvedorContextoEvento>
+      <Pagina>{props.children}</Pagina>
+    </ProvedorContextoEvento>
+  );
 }
